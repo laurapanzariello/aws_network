1916a0ba154300ab2fcf63c431e27e7431e194b5
laura.panzariello <laura.panzariello@corp.globo.com>
2021-03-14 19:50:09 -0300

    create vpc and subnets

--- resources.tf
+++ resources.tf
@@ -1,8 +1,27 @@
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_vpc" "vpc_a" {
+  cidr_block = "10.10.0.0/16"
+}
 
-  tags = {
-    Name = "ExampleInstance"
-  }
+resource "aws_subnet" "subnet_a_1" {
+  vpc_id = "${aws_vpc.vpc_a.id}"
+  cidr_block = "10.10.0.0/18"  
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+}
+
+resource "aws_subnet" "subnet_a_2" {
+  vpc_id = "${aws_vpc.vpc_a.id}"
+  cidr_block = "10.10.64.0/18"    
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"  
+}
+
+resource "aws_subnet" "subnet_a_3" {
+  vpc_id = "${aws_vpc.vpc_a.id}"
+  cidr_block = "10.10.128.0/18"    
+  availability_zone = "${data.aws_availability_zones.available.names[2]}"  
+}
+
+resource "aws_subnet" "subnet_a_4" {
+  vpc_id = "${aws_vpc.vpc_a.id}"
+  cidr_block = "10.10.192.0/18"    
+  availability_zone = "${data.aws_availability_zones.available.names[3]}" 
 }
