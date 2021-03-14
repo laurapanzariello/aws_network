a063281bc1f968355711fb0fa74c437434b1bec2
laura.panzariello <laura.panzariello@corp.globo.com>
2021-03-14 19:51:19 -0300

    create subnets using count and cidrsubnet()

--- resources.tf
+++ resources.tf
@@ -2,26 +2,12 @@ resource "aws_vpc" "vpc_a" {
   cidr_block = "10.10.0.0/16"
 }
 
-resource "aws_subnet" "subnet_a_1" {
+resource "aws_subnet" "public_subnet" {
+  count = 4
   vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "10.10.0.0/18"  
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-}
-
-resource "aws_subnet" "subnet_a_2" {
-  vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "10.10.64.0/18"    
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"  
-}
-
-resource "aws_subnet" "subnet_a_3" {
-  vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "10.10.128.0/18"    
-  availability_zone = "${data.aws_availability_zones.available.names[2]}"  
-}
-
-resource "aws_subnet" "subnet_a_4" {
-  vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "10.10.192.0/18"    
-  availability_zone = "${data.aws_availability_zones.available.names[3]}" 
+  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 2, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  tags = {
+    "Name" = "public_subnet_${data.aws_availability_zones.available.names[count.index]}"
+  }
 }
