07b4254d5fae56f6b72423c02c960df38f833a2e
laura.panzariello <laura.panzariello@corp.globo.com>
2021-03-14 19:43:03 -0300

    create terraform file

---
+++ terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
