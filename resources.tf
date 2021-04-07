225c411c5b676c19c672ad4f4e1d47ead0e2e3e7
Laura Panzariello <laurapanzariello@M4Us-MacBook-Pro.local>
2021-04-07 13:23:00 -0300

    fix subnet overlap

--- resources.tf
+++ resources.tf
@@ -1,11 +1,14 @@
 resource "aws_vpc" "vpc_a" {
   cidr_block = "10.10.0.0/16"
+  tags = {
+    "Name" = "VPC_A"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
   count = 4
   vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 2, count.index)}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 4, count.index)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   tags = {
     "Name" = "public_subnet_${data.aws_availability_zones.available.names[count.index]}"
@@ -15,7 +18,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   count = 4
   vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 2, count.index)}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 4, count.index+4)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   tags = {
     "Name" = "private_subnet_${data.aws_availability_zones.available.names[count.index]}"
@@ -25,7 +28,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "private_db_subnet" {
   count = 4
   vpc_id = "${aws_vpc.vpc_a.id}"
-  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 2, count.index)}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc_a.cidr_block, 4, count.index+8)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   tags = {
     "Name" = "private_db_subnet_${data.aws_availability_zones.available.names[count.index]}"
