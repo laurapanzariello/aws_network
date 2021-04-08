c8345388a2c6d670f55043ebf2851a2e4f45b81a
Laura Panzariello <laurapanzariello@M4Us-MacBook-Pro.local>
2021-04-08 13:35:45 -0300

    create and accept a vpc peering connection

--- resources.tf
+++ resources.tf
@@ -1,3 +1,5 @@
+### VPC A
+
 resource "aws_vpc" "vpc_a" {
   cidr_block = "${var.vpc_cidr[0]}"
   tags = {
@@ -59,7 +61,7 @@ resource "aws_subnet" "private_db_subnet" {
   }
 }
 
-###
+### VPC B
 
 resource "aws_vpc" "vpc_b" {
   cidr_block = "${var.vpc_cidr[1]}"
@@ -120,4 +122,38 @@ resource "aws_subnet" "private_db_subnet_b" {
   tags = {
     "Name" = "private_db_subnet_${data.aws_availability_zones.available.names[count.index]}"
   }
+}
+
+### Peering
+
+data "aws_route_tables" "owner" {
+  vpc_id = "${aws_vpc.vpc_a.id}"
+}
+
+data "aws_route_tables" "accepter" {
+  vpc_id = "${aws_vpc.vpc_b.id}"
+}
+
+resource "aws_vpc_peering_connection" "peering_vpc_a_to_b" {
+  peer_vpc_id = "${aws_vpc.vpc_b.id}"
+  vpc_id = "${aws_vpc.vpc_a.id}"
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer_accept" {
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering_vpc_a_to_b.id}"
+  auto_accept = true
+}
+
+resource "aws_route" "owner" {
+  count = "${length(data.aws_route_tables.owner.ids)}"
+  route_table_id = "${data.aws_route_tables.owner.ids[count.index]}"
+  destination_cidr_block = "${var.vpc_cidr[1]}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering_vpc_a_to_b.id}" 
+}
+
+resource "aws_route" "accepter" {
+  count = "${length(data.aws_route_tables.accepter.ids)}"
+  route_table_id = "${data.aws_route_tables.accepter.ids[count.index]}"
+  destination_cidr_block = "${var.vpc_cidr[0]}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering_vpc_a_to_b.id}" 
 }
